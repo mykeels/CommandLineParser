566737d8259bb5629b4cfd39d06a3fa5a6c6becf
Ikechi Michael <mykehell123@gmail.com>
2017-07-04 19:45:18 +0100

    create flag attribute

---
+++ CommandLineParser/Attributes/FlagAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CommandLineParser.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+    public sealed class FlagAttribute : Attribute
+    {
+        private readonly string shortName;
+        private readonly string name;
+        public readonly bool required;
+        
+        public FlagAttribute(string name = null, string shortName = null, bool required = false)
+        {
+            this.name = name;
+            this.required = required;
+            this.shortName = shortName;
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public string ShortName
+        {
+            get { return shortName; }
+        }
+
+        public bool Required
+        {
+            get { return required; }
+        }
+    }
+}
