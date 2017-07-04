d515f598052c1724676f2d74e367db6417610837
Ikechi Michael <mykehell123@gmail.com>
2017-07-04 14:41:43 +0100

    Add project files.

---
+++ CommandLineParser/Attributes/HelpAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLineParser.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = true)]
+    public sealed class HelpAttribute : Attribute
+    {
+        readonly string usageText;
+
+        // This is a positional argument
+        public HelpAttribute(string usageText)
+        {
+            this.usageText = usageText;
+        }
+
+        public string Usage
+        {
+            get { return usageText; }
+        }
+    }
+}
