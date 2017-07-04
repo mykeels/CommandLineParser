cbcd514cd2c33094e67b371dbf46b6ceb32e30d6
Ikechi Michael <mykehell123@gmail.com>
2017-07-04 19:46:09 +0100

    add test console project

---
+++ CommandLineParser.Console/Models/SetupModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLineParser;
+using CommandLineParser.Attributes;
+using CommandLineParser.Helpers;
+
+namespace CommandLineParser.Console.Models
+{
+    [Help("This is a Command Line Parser Library")]
+    public class SetupModel
+    {
+        [Flag(shortName: "n", required: true)]
+        [Help("The name of the User")]
+        public string Name { get; set; }
+
+        [Flag("free", "f")]
+        [Help("This user is not enslaved")]
+        public bool IsFree { get; set; }
+
+        [Help("The user's age")]
+        public int Age { get; set; }
+
+        [Flag("address", "a", false)]
+        [Help("The user's address")]
+        [Transform(typeof(SetupModel), nameof(_addressTransform))]
+        public AddressModel Address { get; set; }
+
+        public static AddressModel _addressTransform(string address)
+        {
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<AddressModel>(address);
+        }
+    }
+
+    public class AddressModel
+    {
+        public string HomeAddress { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+    }
+}
---
+++ CommandLineParser.Console/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLineParser;
+using CommandLineParser.Console.Models;
+using Newtonsoft.Json;
+
+namespace CommandLineParser.Console
+{
+    public partial class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args != null && args.Length > 0)
+            {
+                CommandParser parser = new CommandParser(args);
+                Console.WriteLine(parser.GetHelpInfo<SetupModel>(), ConsoleColor.White);
+            }
+            else
+            {
+                Program p = new Program();
+                p.testNameHelpInfo();
+            }
+            
+            Console.Read();
+        }
+
+        private void testArgumentParser()
+        {
+            Console.WriteLine(new CommandParser().GetDictionary(new string[] { "--name", "Ikechi Michael", "--empty", "true" }));
+        }
+
+        private void testCliParser()
+        {
+            CommandParser parser = new CommandParser(new string[] { "-n", "Ikechi Michael", "-f", "true", "--age", "13", "--address", JsonConvert.SerializeObject(new AddressModel()) });
+            Console.WriteLine(parser.Parse<SetupModel>());
+        }
+
+        private void testHelpInfo()
+        {
+
+            CommandParser parser = new CommandParser(new string[] { "--help" });
+            //var setup = parser.Parse<SetupModel>();
+            Console.WriteLine(parser.GetHelpInfo<SetupModel>(), ConsoleColor.White);
+        }
+
+        private void testNameHelpInfo()
+        {
+
+            CommandParser parser = new CommandParser(new string[] { "-n", "--help" });
+            Console.WriteLine(parser.GetHelpInfo<SetupModel>(), ConsoleColor.White);
+        }
+    }
+}
