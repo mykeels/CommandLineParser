cfaf4ecfba0213ff1d4fe56d1e316a80b1bf6133
Ikechi Michael <mykehell123@gmail.com>
2017-09-03 14:13:36 +0100

    fix: automatically get csproj file-name and assembly-name

--- CommandLineParser/Nuget/build.bat
+++ CommandLineParser/Nuget/build.bat
@@ -2,3 +2,5 @@ REM delete existing nuget packages
 del *.nupkg
 powershell -noexit -executionpolicy bypass -File "version-no.ps1"
 pause
+
+REM  "../CommandLineParser.csproj" "../bin/debug/CommandLine.Parser.dll"
